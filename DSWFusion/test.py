2d8fdcc7b42075159db46abcb8dc0923644af0b0
XAUTjl <598515257@qq.com>
2025-09-28 16:58:49 +0800

    Add files via upload

---
+++ DSWFusion/test.py
@@ -0,0 +1,65 @@
+from net import Encoder, Decoder, SFEM_S, BaseFeatureExtraction, DetailFeatureExtraction, Sep
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+from utils.img_read_save import img_save,image_read_cv2
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+ckpt_path=r"models/best_model.pth"
+for dataset_name in ["TNO"]:
+    print("The test result of "+dataset_name)
+    test_folder=os.path.join('test_img',dataset_name)
+    test_out_folder=os.path.join('test_result',dataset_name)
+
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    Encoder = nn.DataParallel(Encoder()).to(device)
+    Decoder = nn.DataParallel(Decoder()).to(device)
+    SFEM_S = nn.DataParallel(SFEM_S()).to(device)
+    Sepnet = nn.DataParallel(Sep()).to(device)
+    BaseFuseLayer = nn.DataParallel(BaseFeatureExtraction(dim=64, num_heads=8)).to(device)
+    DetailFuseLayer = nn.DataParallel(DetailFeatureExtraction(num_layers=1)).to(device)
+
+
+    Encoder.load_state_dict(torch.load(ckpt_path)['Encoder'])
+    Decoder.load_state_dict(torch.load(ckpt_path)['Decoder'])
+    SFEM_S.load_state_dict(torch.load(ckpt_path)['SFEM_S'])
+    BaseFuseLayer.load_state_dict(torch.load(ckpt_path)['BaseFuseLayer'])
+    DetailFuseLayer.load_state_dict(torch.load(ckpt_path)['DetailFuseLayer'])
+
+
+    Encoder.eval()
+    Decoder.eval()
+    SFEM_S.eval()
+    Sepnet.eval()
+    BaseFuseLayer.eval()
+    DetailFuseLayer.eval()
+
+
+    with torch.no_grad():
+        for img_name in os.listdir(os.path.join(test_folder,"ir")):
+
+            IR = image_read_cv2(os.path.join(test_folder,"ir",img_name),mode='GRAY')[np.newaxis,np.newaxis, ...]/255.0
+            VI = image_read_cv2(os.path.join(test_folder,"vi",img_name), mode='GRAY')[np.newaxis,np.newaxis, ...]/255.0
+
+
+            IR,VI = torch.FloatTensor(IR),torch.FloatTensor(VI)
+            VI, IR = VI.cuda(), IR.cuda()
+
+
+            # Feature Interaction Extraction and Integration Process
+            feature_V_B, feature_V_D, feature_V, feature_V0 = Encoder(VI)
+            feature_I_B, feature_I_D, feature_I, feature_I0 = Encoder(IR)
+            # Semantic Feature Enhancement Process
+            feature_V_Dd = SFEM_S(feature_V_D)
+            feature_I_bd = SFEM_S(feature_I_B)
+            feature_F_B = BaseFuseLayer(feature_I_bd + feature_V_B)
+            feature_F_D = DetailFuseLayer(feature_I_D + feature_V_Dd)
+            # Feature Fusion Process
+            FuseI, _ = Decoder(VI, feature_F_B, feature_F_D)
+
+
+            FuseI = (FuseI - torch.min(FuseI)) / (torch.max(FuseI) - torch.min(FuseI))
+            fi = np.squeeze((FuseI * 255).cpu().numpy())
+            img_save(fi, img_name.split(sep='.')[0], test_out_folder)
